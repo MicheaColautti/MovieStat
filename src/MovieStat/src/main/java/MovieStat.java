5e84baca6897899f47fba08f8ad2ca0928d63509
Michea Colautti <61267757+MicheaColautti@users.noreply.github.com>
2025-02-21 17:39:50 +0100

    Create MovieStat.java
    Main class. Creating list and populating it

---
+++ src/MovieStat/src/main/java/MovieStat.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class MovieStat {
+
+    public static void main(String[] args) {
+
+        MovieList movieList = new MovieList();
+        movieList.pupulate();
+        System.out.println(movieList);
+
+    }
+}
